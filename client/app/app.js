a542d9649919bf479532bf0358f95b8564b8a24e
Gabo Lugo <gabriel.lugo@globant.com>
2017-11-08 15:29:36 -0600

    Updated angular-ui-router to @uirouter/angularjs

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,6 @@
 import 'normalize.css';
 import angular from 'angular';
-import uiRouter from 'angular-ui-router';
+import uiRouter from '@uirouter/angularjs';
 
 import Common from './common/common';
 import Components from './components/components';
